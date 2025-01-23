a7bf4c83720bdc6fcf9e67896a321ec7c6bdaa43
Kithmal Wimalaratne <wimalaratnek@student.douglascollege.ca>
2025-01-23 03:35:56 -0800

    init: stable version of cottonjs framework

---
+++ route.config.js
@@ -0,0 +1,16 @@
+/**
+ * Routes for your app
+ * @type {import("cottonjs").Routes}
+ */
+const routes = {
+  "/": {
+    name: "home",
+    page: "src/Home",
+  },
+  "/user/:id": {
+    name: "home",
+    page: "src/Home",
+  },
+};
+
+export default routes;
